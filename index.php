da66b88c77a7cb410d613175f10f5d9a701f7cf8
NekoNapster <mdmgrafica@gmail.com>
2023-02-09 11:58:25 +0100

    estilos basicos mas logo

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- IMPORT CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <!-- IMPORT FONT AWESOM -->
+    <script src="https://kit.fontawesome.com/47c31c3b1d.js" crossorigin="anonymous"></script>
+
+    <title>Little Dragon</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <h1 class="h1">Pagina Index de Little Dragon: OK</h1>
+        <i class="fa-solid fa-dragon"></i>
+    </div>
+</body>
+
+</html>
